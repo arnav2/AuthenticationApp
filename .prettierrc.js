5ec1c358e4cabadadba6c8b1969821bd037af57b
Arnav Goel <arnav@hypercomply.com>
2023-07-12 17:07:08 -0700

    Adding initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+export const arrowParens = 'avoid';
+export const bracketSameLine = true;
+export const bracketSpacing = false;
+export const singleQuote = true;
+export const trailingComma = 'all';
+export const tabWidth = 2;
+export const useTabs = false;
---
+++ App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Provider} from 'react-native-paper';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import App from './src';
+import {theme} from './src/core/theme';
+import { Amplify } from 'aws-amplify';
+import {withAuthenticator} from 'aws-amplify-react-native';
+import config from './src/aws-exports';
+
+Amplify.configure(config);
+
+const Main = () => (
+  <SafeAreaProvider>
+  <Provider theme={theme}>
+    <App />
+  </Provider>
+  </SafeAreaProvider>
+);
+
+export default Main;
